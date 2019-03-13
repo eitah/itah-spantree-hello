89538fc790bbd180c2bdfab0acec5d92c5667134
eitah <elijahbit@gmail.com>
2019-03-13 11:48:14 -0500

    Working validator

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const chalk = require('chalk');
 
 const Game = class {
     constructor () {
+        this.numberOfColumns = 7;
         this.board = [
             ['-','-','-','-','-','-','-'],
             ['-','-','-','-','-','-','-'],
@@ -14,16 +15,17 @@ const Game = class {
     }
 
     paintBoard() {
+        console.log(' 1 | 2 | 3 | 4 | 5 | 6 | 7 ');
         this.board.forEach(rowArray => {
             const row = rowArray.reduce((agg, space, index) => {
-                let row = `${agg} ${space}`
+                let row = `${agg} ${space}`;
                 
                 if (index !== (rowArray.length - 1)) {
                     row += ` |`;
                 }
     
                 return row
-            }, '')
+            }, '');
             console.log(row);
         })
     }
@@ -73,30 +75,34 @@ const Game = class {
     }
 }
 
-const turnPrompt = {
-    type: 'input',
-    name: 'space',
-    message: `${chalk.red('Place a piece in column:')}`
-}
+
 
 async function takeTurn(myGame) {
+    const turnPrompt = {
+        type: 'input',
+        name: 'space',
+        validate: (mySpace) => {
+
+            if (mySpace <= myGame.numberOfColumns && mySpace > 0) {
+                return true;
+            }
+            console.log('\nnope, try again with a valid space\n')
+            return false;
+        },
+        message: `${chalk.red('Place a piece in column:')}`
+    };
     const input = await inquirer.prompt(turnPrompt);
-    const mySpace = input.space * 1;
-    if (mySpace < myGame.board[0].length && mySpace > 0) {
-        const result = myGame.placeToken(mySpace);
-        if (!result) {
-            console.log('Please chose a different move')
-            return false
-        }
-    } else {
-        throw new Error('space is invalid')
-        return false;
+    const mySpace = input.space * 1 -1; // convert to number and index value
+    const result = myGame.placeToken(mySpace);
+    if (!result) {
+        console.log('Please chose a different move')
+        return false
     }
     return true;
 }
 
 async function startGame() {
-    console.log('welcome to the game!\n')        
+    console.log('welcome to the game!\n');
     const myGame = new Game();
     myGame.paintBoard();
     try {
@@ -105,7 +111,7 @@ async function startGame() {
             if (result) {
                 myGame.aiMakeMove();
             }
-        } while (!myGame.isBoardFull())
+        } while (!myGame.isBoardFull());
         console.log('If the code made it here the board is Full! Goodbye!');
     } catch (e) {
         console.log(`a bad thing happened ${e}`)
